3cb156d9f26d1ea4166d6322789fd563141a1e11
Obed236 <gellytassango@gmail.com>
2026-08-16 20:19:08 +0200

    Register creator profile page

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import os
 from core import app, GAMES, db_connection, current_pseudo, init_db
 from engagement import register_engagement
 from platform_routes import register_platform_routes
+from creator_routes import register_creator_routes
 
 register_platform_routes(app, GAMES, db_connection, current_pseudo)
 register_engagement(app, GAMES, db_connection, current_pseudo)
+register_creator_routes(app, current_pseudo)
 
 if __name__ == "__main__":
     init_db()
