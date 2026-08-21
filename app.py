60ff885eb61acbc751636d2338fb03b61aa0a88e
Obed Gelly Ousmane Tassango <gellytassango@gmail.com>
2026-08-22 01:21:03 +0200

    Register PlayBed V3 growth features

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 
 from core import app, GAMES, db_connection, current_pseudo, init_db
 from engagement import register_engagement
+from growth import register_growth
 from platform_routes import register_platform_routes, GUIDES
 from editorial_guides import EDITORIAL_GUIDES
 from creator_routes import register_creator_routes
@@ -11,6 +12,7 @@ GUIDES.update(EDITORIAL_GUIDES)
 
 register_platform_routes(app, GAMES, db_connection, current_pseudo)
 register_engagement(app, GAMES, db_connection, current_pseudo)
+register_growth(app, GAMES, db_connection, current_pseudo)
 register_creator_routes(app, current_pseudo)
 register_sitemap_route(app, GAMES, current_pseudo)
 
