b47d5dce278ffbe0cf1cb5e1a342f4e13bcb93d8
Obed Gelly Ousmane Tassango <gellytassango@gmail.com>
2026-09-06 01:13:18 +0200

    security: keep debug disabled in application entrypoint

--- app.py
+++ app.py
@@ -268,4 +268,4 @@ register_security(app, db_connection)
 
 if __name__ == "__main__":
     init_db()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=False)
