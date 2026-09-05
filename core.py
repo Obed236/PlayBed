d1526c01ed9ea3783ae032cc245559e8686722a6
Obed Gelly Ousmane Tassango <gellytassango@gmail.com>
2026-09-06 01:12:52 +0200

    security: disable Flask debug mode

--- core.py
+++ core.py
@@ -628,4 +628,4 @@ def health():
 
 if __name__ == "__main__":
     init_db()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)), debug=False)
